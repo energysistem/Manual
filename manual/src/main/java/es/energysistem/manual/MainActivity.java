6bfe5b431b595d34fabe4d0bcfa22ead9fe75a65
gualde <gualde@gmail.com>
2014-05-15 18:29:08 +0200

    Test conexión + traducciones

--- manual/src/main/java/es/energysistem/manual/MainActivity.java
+++ manual/src/main/java/es/energysistem/manual/MainActivity.java
@@ -2,7 +2,12 @@ package es.energysistem.manual;
 
 import android.app.Activity;
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -27,45 +32,65 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(isOnline()) {
+            WebView webView = (WebView) findViewById(R.id.webView);
+            // Enable JavaScript
+            WebSettings webSettings = webView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            webView.setWebChromeClient(new WebChromeClient());
+            try {
+                Idioma idioma = Idioma.valueOf(Locale.getDefault().getCountry());
+                Toast.makeText(this, idioma.toString(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this,Locale.getDefault().getCountry(), Toast.LENGTH_SHORT).show();
 
-        WebView webView=(WebView)findViewById(R.id.webView);
-        // Enable JavaScript
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webView.setWebChromeClient(new WebChromeClient());
-        try {
-            Idioma idioma = Idioma.valueOf(Locale.getDefault().getCountry());
-            Toast.makeText(this, idioma.toString(), Toast.LENGTH_SHORT).show();
-            //Toast.makeText(this,Locale.getDefault().getCountry(), Toast.LENGTH_SHORT).show();
+                switch (idioma) {
+                    case ES:
+                        webView.loadUrl("http://www.energysistem.com/guide/39530");
+                        break;
+                    case EN:
+                        webView.loadUrl("http://www.energysistem.com/en/guide/39530");
+                        break;
+                    case PT:
+                        webView.loadUrl("http://www.energysistem.com/pt/guide/39530");
+                        break;
+                    case FR:
+                        webView.loadUrl("http://www.energysistem.com/fr/guide/39530");
+                        break;
 
-            switch (idioma) {
-                case ES:
-                    webView.loadUrl("http://www.energysistem.com/guide/39530");
-                    break;
-                case EN:
-                    webView.loadUrl("http://www.energysistem.com/en/guide/39530");
-                    break;
-                case PT:
-                    webView.loadUrl("http://www.energysistem.com/pt/guide/39530");
-                    break;
-                case FR:
-                    webView.loadUrl("http://www.energysistem.com/fr/guide/39530");
-                    break;
-
-                default:
-                    webView.loadUrl("http://www.energysistem.com/en/guide/39530");
-                    break;
+                    default:
+                        webView.loadUrl("http://www.energysistem.com/en/guide/39530");
+                        break;
+                }
+            } catch (Exception error) {
+                webView.clearCache(true);
+                webView.loadUrl("http://www.energysistem.com/en/guide/39530");
             }
         }
-        catch (Exception error)
+        else
         {
-            webView.clearCache(true);
-            webView.loadUrl("http://www.energysistem.com/en/guide/39530");
+            new AlertDialog.Builder(this)
+                    .setTitle(getResources().getString(R.string.AlertTitle))
+                    .setMessage(getResources().getString(R.string.textAlert))
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            closeActivity();
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
         }
-
-
-
-
+    }
+    private void closeActivity()
+    {
+        this.finish();
+    }
+    public boolean isOnline() {
+        ConnectivityManager cm =(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+        return false;
     }
 
 
